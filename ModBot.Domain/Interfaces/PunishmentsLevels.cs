5f26f489908c35bfd8148c9222209a462262da0a
Moosewell <max@melander.biz>
2021-05-19 15:01:12 +0200

    User & punishmentLevels interfaces and folders

---
+++ ModBot.Domain/Interfaces/PunishmentsLevels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModBot.Domain.Interfaces
+{
+    public interface PunishmentsLevels
+    {
+        int TimeOut { get; set; }
+        int Kick { get; set; }
+        int Ban { get; set; }
+    }
+}
