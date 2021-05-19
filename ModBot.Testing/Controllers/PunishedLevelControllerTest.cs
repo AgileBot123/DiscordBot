8a5c97a022d3d95a0ab55ae99300f845f299821c
Nahd92 <dhan.marcellus@hotmail.com>
2021-05-19 16:13:22 +0200

    Added some files in Testing

--- ModBot.Testing/Controllers/PunishedLevelControllerTest.cs
+++ ModBot.Testing/Controllers/PunishedLevelControllerTest.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModBot.API.Controllers;
+using ModBot.Domain.Interfaces;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ModBot.Testing.Controllers
 {
-    class PunishedLevelControllerTest
+    [TestClass]
+    public class PunishedLevelControllerTest
     {
+        private readonly Mock<IPunishmentsLevels> _mockPunish;
+        private PunishedLevelsController _punishedLevelsController;
+        public PunishedLevelControllerTest()
+        {
+            _mockPunish = new Mock<IPunishmentsLevels>();
+            _punishedLevelsController = new PunishedLevelsController();
+        }
     }
 }
