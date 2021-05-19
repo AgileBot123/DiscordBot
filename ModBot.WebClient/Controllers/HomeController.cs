fc03ab2bfd2d1981b0a7a9f75e2bbf6d9c255b75
TheNimum <mikael.pajari@live.se>
2021-05-19 14:11:16 +0200

    Creating HomeController to Client.

---
+++ ModBot.WebClient/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModBot.WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult User_Loggin(string username, string Password)
+        {
+            return View();
+        }
+
+    }
+}
