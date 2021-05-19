36625167e71a038d678a179e3914144d4de5addf
luimar96 <luisa_martensson@hotmail.com>
2021-05-19 15:03:02 +0200

    Controller

---
+++ ModBot.API/Controllers/BannedWordsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModBot.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannedWordsController : ControllerBase
+    {
+        public BannedWordsController()
+        {
+
+        }
+
+        public IActionResult GetBannedWord()
+        {
+            return Ok();
+        }
+        public IActionResult GetAllBannedWords()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBannedWord()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBannedWord()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBannedWord()
+        {
+            return Ok();
+        }
+    }
+}
