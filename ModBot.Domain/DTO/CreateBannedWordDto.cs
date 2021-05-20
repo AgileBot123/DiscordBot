1914e1e3a4ddd932f8742c22c4beea9b6c6b11d7
luimar96 <luisa_martensson@hotmail.com>
2021-05-20 14:38:50 +0200

    interfaces repository

--- ModBot.Domain/DTO/CreateBannedWordDto.cs
+++ ModBot.Domain/DTO/CreateBannedWordDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ModBot.Domain.DTO
 {
-    class CreateBannedWordDto
+    public class CreateBannedWordDto
     {
 
     }
---
+++ ModBot.Domain/Interfaces/RepositoryInterfaces/IChangeLogIRepository.cs
@@ -0,0 +1,16 @@
+﻿using ModBot.Domain.DTO;
+using ModBot.Domain.Interfaces.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModBot.Domain.Interfaces.RepositoryInterfaces
+{
+    public interface IChangeLogIRepository
+    {
+        void Get(int id);
+        IEnumerable<IChangelogService> GetAll();
+        void Create(CreateLogDto createLog);
+        void Delete(int id);
+    }
+}
