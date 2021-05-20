32adb07a6866c471180e960c14479dd89e933124
luimar96 <luisa_martensson@hotmail.com>
2021-05-20 10:35:42 +0200

    dtos

--- ModBot.API/Startup.cs
+++ ModBot.API/Startup.cs
@@ -13,10 +13,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
+
 namespace ModBot.API
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
--- ModBot.Domain/DTO/CreatePunishedDto.cs
+++ ModBot.Domain/DTO/CreatePunishedDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ModBot.Domain.DTO
 {
-    public class CreateBannedWordDto
+    public class CreatePunishedDto
     {
 
     }
