b0945d649e3c88f801e5fc5edf2feb1555110f3f
Nahd92 <dhan.marcellus@hotmail.com>
2021-05-21 14:42:52 +0200

    Added some tests

--- ModBot.Testing/Controllers/PunishedLevelControllerTest.cs
+++ ModBot.Testing/Controllers/PunishedLevelControllerTest.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModBot.API.Controllers;
 using ModBot.Domain.Interfaces;
 using ModBot.Domain.Interfaces.ServiceInterface;
+using ModBot.Domain.Models;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -14,12 +17,12 @@ namespace ModBot.Testing.Controllers
     public class PunishedLevelControllerTest
     {
         private readonly Mock<IPunishmentsLevelsService> _mockPunish;
-        private PunishedLevelsController _punishedLevelsController;
-        //public PunishedLevelControllerTest()
-        //{
-        //    _mockPunish = new Mock<IPunishmentsLevelsService>();
-        //    _punishedLevelsController = new PunishedLevelsController(_mockPunish.Object);
-        //}
+        private PunishedLevelsController punishedLevelsController;
+        public PunishedLevelControllerTest()
+        {
+          _mockPunish = new Mock<IPunishmentsLevelsService>();
+          punishedLevelsController = new PunishedLevelsController(_mockPunish.Object);
+        }
 
 
         [TestMethod]
@@ -57,10 +60,14 @@ namespace ModBot.Testing.Controllers
         public async Task GetPunishedLevel_ShouldReturnOk()
         {
             //Arrange
-
+            var id = 1;
+            _mockPunish.Setup(x => x.GetPunishedLevel(It.IsAny<int>())).ReturnsAsync(new PunishmentsLevels(1, 2, 3, 1, default(DateTime), default(DateTime)));
             //Act
-
+            var response = await punishedLevelsController.GetPunishedLevel(id);
             //Assert
+            var result = response.Should().BeOfType<OkObjectResult>().Subject;
+            var okValue = result.Should().BeOfType<PunishmentsLevels>().Subject;
+            okValue.Id.Should().Be(1);
         }
 
         [TestMethod]
@@ -91,5 +98,37 @@ namespace ModBot.Testing.Controllers
 
             //Assert
         }
+
+        [TestMethod]
+        public async Task CreatePunishedLevel_ShouldReturnBadRequest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public async Task UpdatePunishedLevel_ShouldReturnNoContent()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public async Task UpdatePunishedLevel_ShouldReturnBadRequest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+
     }
 }
