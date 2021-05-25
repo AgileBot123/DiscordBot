bd6c49e0fca55d261f1c30986a2ee2cd67616106
TheNimum <mikael.pajari@live.se>
2021-05-25 10:49:51 +0200

    Hämta Lista av BannedWord

---
+++ ModBot.Domain/DTO/ListOfBannedWordsDTO.cs
@@ -0,0 +1,13 @@
+﻿using ModBot.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static ModBot.Domain.Extensions.Routes.Routes;
+
+namespace ModBot.Domain.DTO
+{
+    public class ListOfBannedWordsDTO
+    {
+        public IEnumerable<BannedWord> BWords { get; set; } = new List<BannedWord>();
+     }
+}
---
+++ ModBot.WebClient/Models/DeleteBannedWordModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModBot.WebClient.Models
+{
+    public class DeleteBannedWordModel
+    {
+        public string Word { get; set; }
+    }
+}
