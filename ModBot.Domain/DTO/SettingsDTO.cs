1987bf54470a1b66c9a580b2cb7c4886c4219030
TheNimum <mikael.pajari@live.se>
2021-05-26 12:50:23 +0200

    SettingsController,  med SettingsModel  & SettingsDTO

---
+++ ModBot.Domain/DTO/SettingsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModBot.Domain.DTO
+{
+    public class SettingsDTO
+    {
+        public string Insert_Word { get; set; }
+        public string BadWord1 { get; set; }
+        public string BadWord2 { get; set; }
+        public string BadWord3 { get; set; }
+        public string Bad_Sentence1 { get; set; }
+        public string Bad_Sentence2 { get; set; }
+    }
+}
