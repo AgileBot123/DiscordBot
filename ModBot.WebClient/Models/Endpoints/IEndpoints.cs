6334a1e8c42a2a5da37a8c6f9c99b059c9e0ba15
luimar96 <luisa_martensson@hotmail.com>
2021-05-27 08:56:36 +0200

    endpoints

---
+++ ModBot.WebClient/Models/Endpoints/IEndpoints.cs
@@ -0,0 +1,38 @@
+﻿namespace ModBot.WebClient.Models.Endpoints
+{
+    public interface IEndpoints
+    {
+        string CreateBannedWord { get; }
+
+        string GetBannedWord { get; }
+
+        string GetAllBannedWords { get; }
+
+        string DeleteBannedWord { get; }
+
+        string UpdateBannedWordList { get; }
+
+        string CreateLog { get; }
+
+        string GetChangeLog { get; }
+
+        string GetAllLogs { get; }
+
+        string DeleteLog { get; }
+
+        string UpdateLog { get; }
+
+        string CreatePunishedLevel { get; }
+
+        string GetPunishedLevel { get; }
+
+        string GetPunishedLevels { get; }
+
+        string DeletePunishedLevel { get; }
+
+        string UpdatePunishedLevel { get; }
+        string GetMember { get; }
+        string GetAllMembers { get; }
+
+    }
+}
